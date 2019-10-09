bd11d2635c3b8f984f791b536c891e1cb04172cc
spelluet <44065511+spelluet@users.noreply.github.com>
2019-10-09 23:04:52 +0200

    Menu
    void menu();
    void play();

--- Crabble/main.c
+++ Crabble/main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    printf("Hello world!\n");
+void menu();
+void play();
+
+int main(int argc, char ** argv){
+    menu();
+    system("pause");
     return 0;
+}
+
+void menu(){
+    int menuBool = 1;
+
+    do{
+        printf( "=========================================================\n"
+                "               BIENVENUE AU CRABBLE\n"
+                "=========================================================\n\n"
+                "Taper :\n"
+                "   1) Pour jouer\n"
+                "   2) Pour quitter\n"
+                );
+
+        fflush(stdin);
+        scanf("%d", &menuBool);
+
+        while(menuBool != 1 && menuBool != 2){
+            system("cls");
+            printf("Taper 1 ou 2");
+            fflush(stdin);
+            scanf("%d", &menuBool);
+        }
+
+        if(menuBool == 1){
+            play();
+        }
+    }while(menuBool != 2);
+}
+
+void play(){
+    system("cls");
+    printf("JOUER !!! ");
+    system("pause");
 }
