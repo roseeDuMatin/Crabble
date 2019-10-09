33df9e0e3cc64385b57e63f33c8047b680e92cc3
spelluet <44065511+spelluet@users.noreply.github.com>
2019-10-09 23:41:07 +0200

    initGrid
    rows et cols

--- Crabble/main.c
+++ Crabble/main.c
@@ -4,6 +4,10 @@
 
 void menu();
 void play();
+char** initGrid(int rows, int cols, int doubleLetters, int tripleLetters, int doubleWords, int tripleWords, int negative);
+void freeDoubleArray(int rows, int cols, char **array);
+
+void printAllocError();
 
 int main(int argc, char ** argv){
     menu();
@@ -40,7 +44,69 @@ void menu(){
 }
 
 void play(){
+    char **grid = NULL;
+    int rows, cols, doubleLetters, tripleLetters, doubleWords, tripleWords, negative;
+
+    // A SCANNER ET VERIFIER
+    // =======================
+    rows = 3;
+    cols = 4;
+    doubleLetters = 1;
+    tripleLetters = 1;
+    doubleWords = 1;
+    tripleWords = 1;
+    negative = 1;
+    // =======================
+    
+    grid = initGrid(rows, cols, doubleLetters, tripleLetters, doubleWords, tripleWords, negative);
+
     system("cls");
     printf("JOUER !!! ");
+    // printGrid();
     system("pause");
+
+    freeDoubleArray(rows, cols, grid);
 }
+
+char** initGrid(int rows, int cols, int doubleLetters, int tripleLetters, int doubleWords, int tripleWords, int negative){
+    int i;
+    //int j;
+    char ** grid = NULL;
+
+    grid = malloc(sizeof(char*) * rows);
+    if(!grid){
+        printAllocError();
+    }
+
+    for(i = 0; i < rows; i++){
+        grid = malloc(sizeof(char) * cols);
+        if(!grid[i]){
+            printAllocError();
+        }
+    }
+
+    // for(i = 0; i < rows; i++){
+    //     for(j = 0; j < cols; j++){
+    //         grid[i][j] = 0;
+    //     }
+    // }
+    return grid;
+}
+
+void freeDoubleArray(int rows, int cols, char **array){
+    int i, j;
+
+    for(i = 0; i < rows; i++){
+        for(j = 0; j < cols; j++){
+            free(*array);
+        }
+    }
+    free(array);
+}
+
+void printAllocError(){
+    printf("\nErreur d'allocation");
+    system("pause");
+    exit(0);
+}
+
--- Crabble/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-
-void play()
-
-
-#endif // MAIN_H_INCLUDED
